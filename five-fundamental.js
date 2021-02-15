19f84535a59e32c8de3ce92887c8d4064de69a5d
unknown <dxshakil2@gmail.com>
2021-02-14 22:53:57 -0800

    done

---
+++ five-fundamental.js
@@ -0,0 +1,66 @@
+
+
+// variable let const ( value change hole let) ( value change na hole let)
+let money = 50;
+    money = 20 
+
+    const isFamouse = false
+    const name = 'sakil' 
+    const fatherName = 'ahzar-ali'
+    
+const person = {
+    name:'shakil',
+    age: 40,
+    address: 'gohailbari'
+}    
+// templeatString   
+ const streatment = `this person ${name} with age ${person.age}`
+
+//  condition
+if(money > 20 && age >= 30 ){
+
+}
+else{
+
+}
+
+// Array
+const numbers = [ 12,33,55,4,33,55]
+const friends = [ 'shakil', 'joba', 'shamin', 'prince']
+const products = [ {name:'leptop', price:500} ,{name:'mobile' ,price:300},{name:'computer', price:10000}]
+
+// Object
+
+const person ={
+    name: 'shakil',
+    age:40,
+    job:'nai',
+    phone:3893,
+    address:'tangail'
+}
+
+// loop forLoop
+
+for (let i = 0; i < numbers.length; i++) {
+    const number = numbers[i];   
+}
+
+// function arrowFunction
+
+function added( num1 , num2){
+    let sum = num1 + num2
+    return
+}
+const result = added(22,18)
+console.log(result)
+
+// arrow
+const add = (num1 ,num2) => num1+num2;
+const twoTime = num =>num*2; 
+const multipule = (x,y) =>{
+    let sum = x+y;
+    let ghun = sum * 2;
+    return ghun /2
+}
+multipule(5,10)
+
